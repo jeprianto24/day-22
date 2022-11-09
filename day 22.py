616b2699ccf79ba2ebce964faa3fc724d146c672
jeprianto24 <115442691+jeprianto24@users.noreply.github.com>
2022-11-09 23:22:19 +0800

    Add files via upload

---
+++ day 22.py
@@ -0,0 +1,5 @@
+bil_awal = int(input('Masukan bilangan awal: '))
+bil_akhir = int(input('Masukan bilangan akhir: '))
+ 
+list_bil = [i for i in range(bil_awal, bil_akhir +1 )]
+print('Daftar bilangan: {}'.format(list_bil))
